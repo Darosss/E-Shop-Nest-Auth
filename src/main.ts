140120564062e9c0f3acb51992d0e2e9b1b5dcba
Darosss <daroing12@gmail.com>
2023-07-20 14:03:17 +0200

    feat: add ValidationPipe to work with RpcExceptions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { RpcException, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './auth/filter/http-exception.filter';
 import { protobufPackage } from './auth/pb/auth.pb';
+import { status } from '@grpc/grpc-js';
 
 async function bootstrap() {
   const app: INestMicroservice = await NestFactory.createMicroservice(
@@ -19,9 +20,28 @@ async function bootstrap() {
     },
   );
 
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      disableErrorMessages: true,
+      exceptionFactory: (errors) => {
+        const errorsList: string[] = [];
+        errors.forEach((x) => {
+          const errValidation = x.constraints;
+          for (const err in errValidation) {
+            errorsList.push(errValidation[err]);
+          }
+        });
+        return new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: errorsList,
+        });
+      },
+    }),
+  );
   await app.listen();
 }
 
