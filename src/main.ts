ac83c1c50ef6e8d0aff9ad4ef24222bb867bb75f
Darosss <daroing12@gmail.com>
2023-10-06 12:03:18 +0200

    refactor: replace hardcoded app url with AUTH_MICROSERVICE_URL

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 import { protobufPackage } from './auth/pb/auth.pb';
 import { status } from '@grpc/grpc-js';
+import { AUTH_MICROSERVICE_URL } from './configs';
 
 async function bootstrap() {
   const app: INestMicroservice = await NestFactory.createMicroservice(
@@ -12,7 +13,7 @@ async function bootstrap() {
     {
       transport: Transport.GRPC,
       options: {
-        url: '0.0.0.0:50051',
+        url: AUTH_MICROSERVICE_URL,
         package: protobufPackage,
         protoPath: join('node_modules/e-shop-nest-proto/proto/auth.proto'),
       },
